32158475859e6d5c716c25ddfe5c9a19383ed67b
Sergio <sergioruza22@gmail.com>
2022-06-21 19:29:27 -0300

    Começando js

---
+++ fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia 1/Agora a prática/scripts.js
@@ -0,0 +1,22 @@
+//5
+const angulo1 = 60;
+const angulo2 = 50;
+const angulo3 = 70;
+let somaAngulo = angulo1 + angulo2 + angulo3;
+
+if (somaAngulo === 180) {
+    console.log(true)
+} else {
+    console(false, "Isso não representa um triângulo")
+}
+
+//8
+const num1 = 22;
+const num2 = 37;
+const num3 = 71;
+
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+    console.log(true)
+} else {
+    console.log(false)
+}
