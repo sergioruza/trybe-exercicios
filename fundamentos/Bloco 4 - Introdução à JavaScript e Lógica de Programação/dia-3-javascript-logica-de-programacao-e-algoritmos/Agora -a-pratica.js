a4115390b8a4381f2c60000995644cd49db7f2cf
Sergio <sergioruza22@gmail.com>
2022-06-24 09:48:48 -0300

    salvando midificações

--- fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-3-javascript-logica-de-programacao-e-algoritmos/Agora -a-pratica.js
+++ fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-3-javascript-logica-de-programacao-e-algoritmos/Agora -a-pratica.js
@@ -18,23 +18,45 @@
 // console.log(invert);
 
 
+// /*
+// * Questão 3
+// */
+// let palavras =  ['Dálmata','Vampiro', 'Paleta', 'Último', 'Cereja', 'Cerveja', 'Esportes', 'Disputa', 'Senado', 'Longe', 
+// 'Molar', 'Anunciar', 'Personagem'];
+// let maiorPalavra = palavras[0];
 
-// Questão 3
+
+// for(let indice3 = 0; indice3 < palavras.length; indice3 += 1) {
+//     if(palavras[indice3].length > maiorPalavra.length) {
+//         maiorPalavra = palavras[indice3];
+//     }
+// }
+// console.log(maiorPalavra)
+
+// let menorPalavra = palavras[0];
+// for(let ind = 0; ind < palavras.length; ind += 1) {
+//     if(palavras[ind].length < menorPalavra.length) {
+//         menorPalavra = palavras[ind];
+//     }
+// }
+// console.log(menorPalavra)
 
 
-// Questão 4
-let array = []
-let primo = []
+/*
+* Questão 4 - tirar dúvida: o pq do true
+*/
+let primoMaior = 0
+
 for(let count = 2; count <= 50; count += 1) {
-    array.push(count);
-    
-    for (let indice2 = 0; indice2 < array.length; indice2 += 1) {
-        if(array[indice2] % indice2 === 0) {
-            return false;
-        } else {
-            primo.push(array[indice2])
+    let primo = true;
+    for(let divide = 2; divide < count; divide += 1) {
+        if(count % divide === 0) {
+            primo = false;
         }
     }
+        if(primo) {
+            primoMaior = count;
+        }
 }
-console.log(primo)
 
+console.log(primoMaior);
