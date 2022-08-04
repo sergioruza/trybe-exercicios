cddf36cc1a8708535f33dbe34b37042d99ce0253
Sergio <sergioruza22@gmail.com>
2022-08-04 18:30:11 -0300

    Exercicios

---
+++ dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/scripts.js
@@ -0,0 +1,53 @@
+// 🚀 4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+// Dica: use object destructuring.
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo
+
+const peoples = (array) => {
+  return array.filter(({nationality, bornIn}) =>{
+    return nationality === 'Australian' && bornIn > 1900 && bornIn <= 2000;
+  })
+}
+// console.log(peoples(people)); 
+
+const rectangleArea = (width, height) => width * height;
+
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
+
+rectangles.forEach((rectangle) => {
+  rectangleArea() // altere a chamada da funcao rectangleArea
+  console.log(rectangle[0] * rectangle[1]);
+});
+
+rectangleArea()
